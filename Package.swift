f57bf597aa3bde7c7eeaf8aff5c5f033440de440
Dan Loman <daniel.h.loman@gmail.com>
2020-02-15 11:01:54 -0800

    Use forked fluent-kit

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         .library(name: "Fluent", targets: ["Fluent"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/fluent-kit.git", from: "1.0.0-beta.2"),
+        .package(url: "https://github.com/namolnad/fluent-kit.git", branch: "parent-eagerload-crash-metadata"),
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-beta.2"),
     ],
     targets: [
